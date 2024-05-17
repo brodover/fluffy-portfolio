5b2e97f7ca3a11a104126cba878069285794c634
mup <brodover@gmail.com>
2024-05-17 17:43:42 +0900

    added home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, OnInit, AfterViewInit, ViewChild, HostListener } from '@angular/core';
+import { RouterOutlet, provideRouter } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+
 import { TopBarComponent } from './top-bar/top-bar.component';
+
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +13,34 @@ import { TopBarComponent } from './top-bar/top-bar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('stickyMenu')
+  menuElement!: ElementRef;
+
+  stuck:boolean = true;
+  elementPosition: any;
+  
   title = 'Rachel\'s-Portfolio';
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+    handleScroll(){
+      const windowScroll = window.scrollY;
+      if(windowScroll >= this.elementPosition){
+        this.stuck = true;
+      } else {
+        this.stuck = false;
+      }
+    }
+
 }
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  /*{
+    path: 'portfolio',
+    component: PortfolioComponent,
+  },*/
+  {
+    path: 'contacts', component: ContactsComponent,
+  },
+];
