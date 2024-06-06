b96bcd4d2407b7675804998daa2801ff625a1f65
mup <brodover@gmail.com>
2024-06-06 21:05:18 +0900

    - updated contact page

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
+  email = "brodover@gmail.com";
+  copied = "Copied!";
+  tooltip = this.email;
+  
+  copyText() {
+    navigator.clipboard.writeText(this.email);
+    
+    this.tooltip = this.copied;
+  }
 
+  resetTooltip() {
+    this.tooltip = this.email;
+  }
 }
