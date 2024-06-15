5987e1615caf424003b35fb9bf28fb03ddc1223c
mup <brodover@gmail.com>
2024-06-16 04:35:35 +0900

    - try fix httpclient for remote

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core
 import { provideRouter } from '@angular/router';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { provideHttpClient } from "@angular/common/http";
 
 import { routes } from './app.routes';
+import { APIInterceptor } from './api-interceptor.service';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -15,6 +16,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: APIInterceptor,
+      multi: true,
+    },
     importProvidersFrom(
       TranslateModule.forRoot({
         defaultLanguage: 'en',
