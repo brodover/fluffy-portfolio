6b30fa4942dc44f6659e38c2034f64ca49a1fefc
mup <brodover@gmail.com>
2024-06-17 23:01:44 +0900

    - fixed remote translation files url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { bootstrapApplication, Title } from '@angular/platform-browser';
-import { appConfig } from './app.config';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { LanguageSwitchComponent } from './language-switch/language-switch.component';
+import defaultLanguage from "./../assets/i18n/en.json";
 
 @Component({
   selector: 'app-root',
@@ -24,6 +23,8 @@ export class AppComponent implements OnInit {
   constructor(private titleService: Title, public translateService: TranslateService) {
     var locale = translateService.getBrowserLang() || this.defaultLocale;
     translateService.setDefaultLang(this.defaultLocale);
+    if (locale == 'en')
+      translateService.setTranslation(locale, defaultLanguage);
     translateService.addLangs(['en', 'ko']);
     this.changeLocalization(locale);
   }
@@ -46,5 +47,3 @@ export class AppComponent implements OnInit {
     }
   }
 }
-
-bootstrapApplication(AppComponent, appConfig);
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,28 +2,22 @@ import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core
 import { provideRouter } from '@angular/router';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { provideHttpClient } from "@angular/common/http";
 
 import { routes } from './app.routes';
-import { APIInterceptor } from './api-interceptor.service';
+import { environment } from '../environments/environment';
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, environment.apiUrl + '/assets/i18n/', '.json');
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: APIInterceptor,
-      multi: true,
-    },
     importProvidersFrom(
       TranslateModule.forRoot({
-        defaultLanguage: 'en',
         loader: {
           provide: TranslateLoader,
           useFactory: createTranslateLoader,
