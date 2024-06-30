aead56b028be64cb2489d6ec512b214a4bc3be5c
mup <brodover@gmail.com>
2024-06-30 18:16:51 +0900

    - fix project url

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { trans } from '../../assets/i18n/trans';
